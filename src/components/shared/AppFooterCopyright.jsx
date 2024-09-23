effe1c68d987939eadc4a45a535c68fafc57e3fc
mrorko840 <mrorko840@gmail.com>
2024-09-24 04:59:31 +0600

    next

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -3,13 +3,11 @@ function AppFooterCopyright() {
 		<div className="font-general-regular flex justify-center items-center text-center">
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
 				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
-					target="__blank"
+				<div
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
 					Thank You
-				</a>
+				</div>
 			</div>
 		</div>
 	);
